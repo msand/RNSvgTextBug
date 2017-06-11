756c61cb2c11e4e695f4e8af21ad63d7e2aad9ea
Mikael Sand <msand@abo.fi>
2017-06-11 19:55:15 +0300

    Simplify reproduction

--- components/path.js
+++ components/path.js
@@ -1,68 +1,33 @@
 import React from "react";
 
 import Svg, {
-    Circle,
-    Image,
-    LinearGradient,
-    Path,
-    Defs,
-    Stop,
-    Rect,
-    Polygon,
-    G,
-    Text,
-    TextPath
+  Path,
+  Defs,
+  Text,
+  TextPath
 } from "svgs";
 
 const Label = ({ name, x }) =>
-    <Text
-        fontSize="8"
-        fontFamily="sans-serif"
-        fill="gray"
-        strokeWidth="10px"
-        x={x}
-        y="16"
-    >
-      <TextPath href="#textcurve">
-          {name.toUpperCase()}
-      </TextPath>
-    </Text>;
+  <Text x={x} y="12">
+    <TextPath href="#textcurve">
+      {name}
+    </TextPath>
+  </Text>;
 
 export default () =>
-    <Svg
-        version="1.1"
-        viewBox="0 0 841.9 595.3"
-        x="0px"
-        y="0px"
-        xmlSpace="preserve"
-        width="450"
-        height="256.9771"
-    >
-      <Defs>
-        <Path
-            id="textcurve"
-            fill="none"
-            stroke="#00f"
-            d="M150 50c55.23 0 100 44.77 100 100s-44.77 100-100 100S50 205.23 50 150 94.77 50 150 50"
-        />
-      </Defs>
-      <G translateX="100" opacity="0.5">
-        <G scale="2.05">
-          <Path
-              fill="none"
-              stroke="#00f"
-              d="M150 50c55.23 0 100 44.77 100 100s-44.77 100-100 100S50 205.23 50 150 94.77 50 150 50"
-          />
-        </G>
-      </G>
-      <G translateX="100" opacity="0.9">
-        <G scale="2.05">
-          <Label x="25" name="Test1" />
-          <Label x="90" name="Test2" />
-          <Label x="180" name="Test3" />
-          <Label x="400" name="Test4" />
-          <Label x="485" name="Test5" />
-          <Label x="575" name="Test6" />
-        </G>
-      </G>
-    </Svg>;
+  <Svg width="600" height="300" viewBox="-150 0 600 300">
+    <Defs>
+      <Path
+        id="textcurve"
+        d="M150 50c55.23 0 100 44.77 100 100s-44.77 100-100 100S50 205.23 50 150 94.77 50 150 50"
+      />
+    </Defs>
+    <Path
+      fill="none"
+      stroke="#00f"
+      d="M150 50c55.23 0 100 44.77 100 100s-44.77 100-100 100S50 205.23 50 150 94.77 50 150 50"
+    />
+    <Label x="25" name="Test 1" />
+    <Label x="180" name="Test 2" />
+    <Label x="485" name="Test 3" />
+  </Svg>;
